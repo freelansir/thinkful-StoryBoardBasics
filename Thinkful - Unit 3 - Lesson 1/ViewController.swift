ce7fce190f062f7110d8abfc9994cba7267adf76
David Kong <dave.a.kong@gmail.com>
2014-10-02 18:21:10 -0500

    Xcode Project using Storyboards - Basic
    Unit 3, Lesson 1

---
+++ Thinkful - Unit 3 - Lesson 1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Thinkful - Unit 3 - Lesson 1
+//
+//  Created by David Kong on 2014-09-27.
+//  Copyright (c) 2014 Thinkful. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
